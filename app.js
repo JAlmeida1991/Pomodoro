7ccc7bad7af5ae3fd4914c5e7f673e2f1ae9d013
Jonathan <jalmeida0291@gmail.com>
2018-08-29 21:53:36 -0400

    refactored logic and improved ui

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const breakMinus = document.querySelector(".break-minus");
 const breakAdd = document.querySelector(".break-add");
 
 // AUDIO FOR TRANSITION
-let audio = new Audio("Bepp-beep/Bepp-beep.mp3");
+const audio = new Audio("Bepp-beep/Bepp-beep.mp3");
 
 // NEEDED TO ASSIGN SET TIMEOUT TO AS GLOBAL VARIABLE IN ORDER TO CLEAR IT
 let interval;
@@ -27,13 +27,14 @@ class Timer {
     this.totalSeconds = totalSeconds;
   }
   minutes() {
-    let minutes = Math.floor((this.totalSeconds % 3600) / 60);
+    const minutes = Math.floor((this.totalSeconds % 3600) / 60);
     return checkSingleDigit(minutes);
   }
   seconds() {
-    let seconds = (this.totalSeconds % 3600) % 60;
+    const seconds = (this.totalSeconds % 3600) % 60;
     return checkSingleDigit(seconds);
   }
+  // Static method for resting time
   static init() {
     countdown.totalSeconds = 1500;
     breakCountdown.totalSeconds = 300;
@@ -45,6 +46,8 @@ class Timer {
   }
 }
 
+// Creating two instances of time class
+
 const countdown = new Timer(1500);
 
 const breakCountdown = new Timer(300);
@@ -55,22 +58,54 @@ let currentTime;
 
 Timer.init();
 
-// Event handlers:
+// Application CONTROLLER for user input... (Event handlers):
 play.addEventListener("click", startCountdown);
 
 pause.addEventListener("click", stopCountdown);
 
 reset.addEventListener("click", Timer.init);
 
-sessionMinus.addEventListener("click", function() {
+sessionMinus.addEventListener("click", deductSessionTime);
+
+sessionAdd.addEventListener("click", addSessionTime);
+
+breakMinus.addEventListener("click", deductBreakTime);
+
+breakAdd.addEventListener("click", addBreakTime);
+
+// Application MODEL for Timer logic...:
+
+function startCountdown() {
+  if (!isOn) {
+    isOn = true;
+    return (interval = setInterval(countdownInterval, 1000));
+  }
+}
+
+function stopCountdown() {
+  isOn = false;
+  clearInterval(interval);
+}
+
+function deductSessionTime() {
+  // Need to be greeter than 60 since otherwise user will decrement total minutes to nothing
   if (countdown.totalSeconds > 60 && !isOn) {
     countdown.totalSeconds -= 60;
     displayTime(sessionLength);
     displayTime(timer);
   }
-});
+}
 
-sessionAdd.addEventListener("click", function() {
+function deductBreakTime() {
+  // Need to be greeter than 60 since otherwise user will decrement total minutes to nothing
+  if (breakCountdown.totalSeconds > 60 && !isOn) {
+    breakCountdown.totalSeconds -= 60;
+    // This will override what is shown from display Time
+    displayCountDown(breakLength);
+  }
+}
+
+function addSessionTime() {
   // If total seconds is set to less than or equal to 3600, user is possible to increment / decrement time if timer value... e.g. 59:40 => +60
   if (countdown.totalSeconds < 3541 && !isOn) {
     countdown.totalSeconds += 60;
@@ -79,26 +114,16 @@ sessionAdd.addEventListener("click", function() {
     // This will override what is shown from display Time
     displaySixty(countdown, sessionLength);
   }
-});
+}
 
-breakMinus.addEventListener("click", function() {
-  if (breakCountdown.totalSeconds > 60 && !isOn) {
-    breakCountdown.totalSeconds -= 60;
-    displayCountDown(breakLength);
-    // This will override what is shown from display Time
-  }
-});
-
-breakAdd.addEventListener("click", function() {
+function addBreakTime() {
   // If total seconds is set to less than or equal to 3600, user is possible to increment / decrement time if timer value... e.g. 59:40 => +60
   if (breakCountdown.totalSeconds < 3541 && !isOn) {
     breakCountdown.totalSeconds += 60;
     displayCountDown(breakLength);
     displaySixty(breakCountdown, breakLength);
   }
-});
-
-// Application logic:
+}
 
 function countdownInterval() {
   currentTime.totalSeconds--;
@@ -114,6 +139,8 @@ function countdownInterval() {
     breakCountdown.totalSeconds = parseInt(breakLength.textContent) * 60;
   }
 }
+
+//Application VIEWS for UI:
 
 function displayTime(time) {
   if (time === sessionLength) {
@@ -137,20 +164,7 @@ function checkSingleDigit(num) {
   return ("0" + num).slice(-2);
 }
 
-function startCountdown() {
-  if (!isOn) {
-    isOn = true;
-    return (interval = setInterval(countdownInterval, 1000));
-  }
-}
-
-function stopCountdown() {
-  isOn = false;
-  clearInterval(interval);
-}
-
-// This helper function is needed since without it current time will be displayed as 00:00
-
+// This helper function is needed since without it current time will be displayed as 00:00 if user want time to be set to 60 minute
 function displaySixty(time, timeIncrementer) {
   if (time.totalSeconds === 3600) {
     timer.textContent = "60:00";
